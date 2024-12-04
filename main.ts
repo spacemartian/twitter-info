24d70d90421cc1f26f2aad94511e6e6fc54d1ac7
spacemartian <espacellama@gmail.com>
2024-12-04 15:31:53 +0000

    Rename main.ts to example.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import TokenMetadataAnalysis from './fetchTokenMetata';
-dotenv.config();
-
-(async () => {
-    const metadata = new TokenMetadataAnalysis(process.env.BEARER_TOKEN!);
-
-    console.log(await metadata.generateSummary("7etsuo", "8i51XNNpGaKaj4G4nDdmQh95v4FKAxw8mhtaRoKd9tE8"))
-})()
